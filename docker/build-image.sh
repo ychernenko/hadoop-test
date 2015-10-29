4ef85211a0f952ee7dd5eba6e04179e67a7781c3
ychernenko <yura4e@gmail.com>
2015-10-29 12:17:42 +0200

    Flume installation

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -6,5 +6,6 @@ cd context
 
 ../download-hadoop.sh
 ../download-java.sh
+../download-flume.sh
 
 docker build --force-rm=true --no-cache=true --rm=true -t $IMAGE_NAME .
--- docker/context/install.sh
+++ docker/context/install.sh
@@ -3,8 +3,6 @@
 set -e
 
 GLOBALS_HOME=/etc/profile.d
-JAVA_ENV=$GLOBALS_HOME/java.sh
-HADOOP_ENV=$GLOBALS_HOME/hadoop.sh
 
 yum -y install which tar openssh-server openssh-clients
 
@@ -27,14 +25,13 @@ echo 'Installing Java ...'
 
 JAVA_DIR=/opt/java
 mkdir $JAVA_DIR
-tar --no-same-owner -xzf jdk-*.tar.gz -C $JAVA_DIR
+tar -xzvf jdk-*.tar.gz -C $JAVA_DIR
 JAVA_HOME=$JAVA_DIR/$(ls $JAVA_DIR)
 
-echo "
+{ source <(tee $GLOBALS_HOME/java.sh); } << EOF
 export JAVA_HOME=$JAVA_HOME
 export PATH=\$PATH:$JAVA_HOME/bin
-" > $JAVA_ENV
-source $JAVA_ENV
+EOF
 
 echo 'Java installed'
 
@@ -43,16 +40,30 @@ echo 'Installing Hadoop ...'
 
 HADOOP_DIR=/opt/hadoop
 mkdir $HADOOP_DIR
-tar --no-same-owner -xzf hadoop-*.tar.gz -C $HADOOP_DIR
+tar -xzvf hadoop-*.tar.gz -C $HADOOP_DIR
 HADOOP_HOME=$HADOOP_DIR/$(ls $HADOOP_DIR)
 
-echo "
+{ source <(tee $GLOBALS_HOME/hadoop.sh); } << EOF
 export HADOOP_HOME=$HADOOP_HOME
 export PATH=\$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
-" > $HADOOP_ENV
-source $HADOOP_ENV
+EOF
 
 echo 'Hadoop installed'
+
+
+echo 'Installing Flume ..'
+
+FLUME_DIR=/opt/flume
+mkdir $FLUME_DIR
+tar -xzvf apache-flume-*.tar.gz -C $FLUME_DIR
+FLUME_HOME=$FLUME_DIR/$(ls $FLUME_DIR)
+
+{ source <(tee $GLOBALS_HOME/flume.sh); } << EOF
+export FLUME_HOME=$FLUME_HOME
+export PATH=\$PATH:$FLUME_HOME/bin:$FLUME_HOME/sbin
+EOF
+
+echo 'Flume installed'
 
 echo 'Copying configs'
 cp -rfv hadoop-home/* $HADOOP_HOME
